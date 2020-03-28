cd96dd35a103207c913b646bca205e00e8ab94d5
reshadbinharun <md.bin_harun@tufts.edu>
2020-03-28 17:50:26 -0400

    added cron jobs for temperature checkin

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 var express = require('express');
 var router = express.Router();
-const MongoClient = require('mongodb').MongoClient;
-//const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}/${process.env.DB}`;
-const uri = "URIstring"
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 router.get('/inputOne', async (req, res) => {
+    client = req.client;
     try {
-        await client.connect();
         await insertSingleUser(client,
             {
                 "phone_num": "1111111111",
@@ -16,11 +12,7 @@ router.get('/inputOne', async (req, res) => {
               }
             );
     } catch (e) {
-        res.json({
-            message: e
-        });
-    } finally {
-        await client.close();
+        next(e);
     }
     res.send('MongoDB Post Made')
 });
