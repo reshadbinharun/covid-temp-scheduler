1997516410854c7324619611b5ea18a189ebe567
Alexander Williams <alexanderwilliams@Alexanders-MacBook-Pro-3.local>
2020-03-28 16:27:54 -0400

    closed connection, readded env template, formatting

--- routes/mongo.js
+++ routes/mongo.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+//const uri = `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}/${process.env.DB}`;
 const uri = "URIstring"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-router.get('/inputone', async (req, res) => {
+router.get('/inputOne', async (req, res) => {
     try {
         await client.connect();
         await insertSingleUser(client,
@@ -18,6 +19,8 @@ router.get('/inputone', async (req, res) => {
         res.json({
             message: e
         });
+    } finally {
+        await client.close();
     }
     res.send('MongoDB Post Made')
 });
