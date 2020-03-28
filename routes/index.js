f855dfeb243ca75d5beb07265d66115b235bd82c
reshadbinharun <md.bin_harun@tufts.edu>
2020-03-28 14:50:44 -0400

    added api to upload form

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require('multer');
+const csv = require('fast-csv');
+const fs = require('fs');
+// handler to work with multi-part form data i.e. files
+const upload = multer({ dest: 'tmp/csv/' });
+
 router.get('/', function(req, res, next) {
   res.send("This is the index page!");
+});
+
+router.post('/ingest/csvFile', upload.single('file'), (req, res, next) => {
+    const filePath = req.file.path;
+    let results = [];
+    fs.createReadStream(filePath)
+    .pipe(csv.parse({ headers: true }))
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      results.push(row)
+    })
+    .on('end', rowCount => {
+      console.log(`Parsed ${rowCount} rows`);
+      // delete temporary file stored in tmp/csv
+      fs.unlinkSync(filePath);
+      res.send({data: results});
+    });
 });
 
 module.exports = router;
