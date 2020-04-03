4278178482d345294644e50b9e79bd0d84ec93f9
reshadbinharun <reshadbinharun@gmail.com>
2020-04-02 23:42:37 -0400

    debug logs

--- cronHelpers.js
+++ cronHelpers.js
@@ -28,7 +28,9 @@ async function getUsers(dbClient) {
 }
 
 async function sendTemperatureCheckin(twilioClient, user, nextCheckIn, period) {
-    const flow = user.prefersCall ? process.env.TWILIO_CALL_CHECKIN_FLOW : process.env.TWILIO_TEXT_CHECKIN_FLOW;
+    const flow = user.reminders ? process.env.TWILIO_CALL_CHECKIN_FLOW : process.env.TWILIO_TEXT_CHECKIN_FLOW;
+    console.log("flow is", flow);
+    console.log("user is", user);
     await twilioClient.studio.v1.flows(flow)
         .executions
         .create(
